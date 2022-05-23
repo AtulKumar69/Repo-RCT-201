4c102c23f51ca74dbc7bf5a9e82a5ce7a229ec88
AtulKumar69 <99542817+AtulKumar69@users.noreply.github.com>
2022-05-23 16:15:59 +0530

    done

--- SPRINT-1/Evalution/hookapp/src/App.tsx
+++ SPRINT-1/Evalution/hookapp/src/App.tsx
@@ -3,18 +3,18 @@ import { useList } from "./hooks/useList";
 import { useCounter } from "./hooks/useCounter";
 
 function App() {
-  const [uselist1, { push, pop, clear, reset, map }]:any = useList([1, 2, 3]);
+  const [list1, { push, pop, clear, reset, map }]:any = useList([1, 2, 3]);
   const { value, increment, decrement, set } = useCounter(10);
   return (
     <div className="App">
-      <button onClick={() => push(uselist1.length + 1)}>APPEND</button>
+      <button onClick={() => push(list1.length + 1)}>APPEND</button>
       <button onClick={() => pop()}>POP</button>
       <button onClick={() => clear()}>CLEAR</button>
       <button onClick={() => reset()}>RESET</button>
       <button onClick={() => map((e: any) => e * 2)}>
         DOUBLE EVERY ELEMENT
       </button>
-      {uselist1.map((ul: any) => (
+      {list1.map((ul: any) => (
         <div key={ul}>{ul}</div>
       ))}
 
--- SPRINT-1/Evalution/hookapp/src/hooks/useList.tsx
+++ SPRINT-1/Evalution/hookapp/src/hooks/useList.tsx
@@ -1,32 +1,32 @@
 import React from "react"
 export const useList = (array:any) => {
 
-    const[uselist1,uselist2]=React.useState(array)
+    const[list1,list2]=React.useState(array)
 
     const push=(x:number)=>{
-        uselist1.push(x)
-        uselist2([...uselist1])
+        list1.push(x)
+        list2([...list1])
     }
     const pop=()=>{
-        uselist1.pop()
-        uselist2([...uselist1])
+        list1.pop()
+        list2([...list1])
     }
 
     const clear=()=>{
-        uselist2([])
+        list2([])
     }
 
     const reset=()=>{
-        uselist2(array)
+        list2(array)
     }
 
     const map=(x:Function)=>{
         console.log(x)
-       let mydata=uselist1.map(x)
-       uselist2(mydata)
+       let mydata=list1.map(x)
+       list2(mydata)
     }
 
 
    let value1={push,pop,clear,reset,map}
-    return[uselist1,value1]
+    return[list1,value1]
 };
