7d70c2ec5a88b14c998dc3c07ce8024da755b867
AtulKumar69 <99542817+AtulKumar69@users.noreply.github.com>
2022-05-26 00:45:55 +0530

    d

---
+++ SPRINT-2/DAY-1/Assignment/tab/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { TabsComp } from './components/Tab';
+
+
+function App() {
+ 
+  return (<>
+    <div className="App">
+     
+    <TabsComp/>
+    </div>
+  </>
+  
+  );
+}
+
+export default App;
